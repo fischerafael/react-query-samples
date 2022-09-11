f390f8ec6092b42bb0b4a2ea6f811ccebf79bd67
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-09-11 12:27:10 -0300

    feat: fetching using custom function

--- src/pages/fetch-data/index.tsx
+++ src/pages/fetch-data/index.tsx
@@ -1,3 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { pokemonAPI } from "../../config/axios";
+
 export const FetchDataPage = () => {
+  const pokemons = useQuery(["pokemons"], async () => {
+    const { data } = await pokemonAPI.get("/pokemon");
+    return data;
+  });
+
+  console.log(pokemons.data);
+
   return <div>FetchDataPage</div>;
 };
